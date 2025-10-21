91abc744c788e9c053a70e9d2f535c967f9a3ac9
Leon Della Paolera <lldellapaolera@gmail.com>
2025-10-21 16:34:00 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,10 @@ import.meta.env.VITE_PUBLIC_URL;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>    
-    <BrowserRouter basename='/TalentoLabReactJSProject'>
+    <BrowserRouter basename='/'>
     <CarritoProvider>        
       <App />          
     </CarritoProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
