fb67ae5494a06c11ec9e47a4cc31f24f6b42e137
Leon Della Paolera <lldellapaolera@gmail.com>
2025-12-15 20:28:18 -0300

    upload rama del proyecto final

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import.meta.env.VITE_PUBLIC_URL;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>    
-    <BrowserRouter basename={"//"}>
+    <BrowserRouter basename={"/TalentoLabReactJSProject/"}>
       <ProductsProvider>
         <CarritoProvider>        
           <App />          
